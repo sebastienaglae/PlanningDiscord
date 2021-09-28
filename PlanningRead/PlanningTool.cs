276795360ff49c5c657915340b4516d7409a8644
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-09-28 22:49:14 +0200

    Adding Working & Free Time

---
+++ PlanningRead/PlanningTool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningRead
+{
+    public class PlanningTool
+    {
+        public static int WorkingTime(List<Subject> subjects)
+        {
+            int minuteWork = 0;
+            foreach (var subject in subjects)
+            {
+                minuteWork += subject.GetDateStart().TimeBetween(subject.GetDateEnd());
+            }
+
+            return minuteWork;
+        }
+
+        public static int FreeTime(List<Subject> subjects)
+        {
+            if (subjects.Count - 1 < 2)
+                return 0;
+
+            int minuteFree = 0;
+            for (int i = 0; i < subjects.Count - 1; i++)
+            {
+                minuteFree += subjects[i].GetDateEnd().TimeBetween(subjects[i + 1].GetDateStart());
+            }
+
+            return minuteFree;
+        }
+    }
+}
