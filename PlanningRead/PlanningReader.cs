117cd1e28c466405405d2a1cf20fbaf13907e3bb
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-09-28 22:49:50 +0200

    Fix methods

--- PlanningRead/PlanningReader.cs
+++ PlanningRead/PlanningReader.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlanningRead
 {
     public class PlanningReader
     {
-        private static String cacheFolder = "cache";
+        private static readonly String cacheFolder = "cache";
+        private List<Subject> subjects;
 
-        public static String GetIcalContent(String url)
+        public PlanningReader(String edtIcs, String version, String idCal, String param)
+        {
+            GetAllSubject(GetIcalContent(edtIcs, version, idCal, param));
+        }
+
+        public static String GetIcalContent(String edtIcs, String version, String idCal, String param)
         {
             if (!Directory.Exists(cacheFolder))
                 Directory.CreateDirectory(cacheFolder);
 
             String icalPath = cacheFolder + "/" + "0.ical";
+            String url = File.ReadAllText("hyperplanningUrl.var") + edtIcs + "?version=" + version + "&idICal=" + idCal + "&param=" + param;
 
             using (var client = new WebClient())
             {
@@ -27,33 +31,83 @@ namespace PlanningRead
             return File.ReadAllText(icalPath);
         }
 
-        public static String GetAllFields(String ical)
+        public static List<String> GetAllFields(String ical)
         {
             String[] splitIcal = ical.Split("\n");
             String start = "BEGIN:VEVENT";
             String end = "END:VEVENT";
 
-            String fields = "";
             bool exportData = false;
+            List<String> fields = new();
             for (int i = 0; i < splitIcal.Length; i++)
             {
                 if (splitIcal[i].Contains(end))
+                {
                     exportData = false;
+                }
                 if (exportData)
                 {
                     String[] splitField = splitIcal[i].Split(":");
-                    fields += splitField[0] + "\n";
+                        if (!fields.Contains(splitField[0]))
+                            fields.Add(splitField[0]);
                 }
                 if (splitIcal[i].Contains(start))
+                {
                     exportData = true;
+                }
             }
 
             return fields;
         }
 
-        public static List<Subject> GetAllSubject(String ical)
+        public static List<String> GetAllValue(String ical, String fields)
         {
-            List<Subject> subjects = new List<Subject>();
+            String[] splitIcal = ical.Split("\n");
+            String start = "BEGIN:VEVENT";
+            String end = "END:VEVENT";
+
+            bool exportData = false;
+            List<String> values = new();
+            for (int i = 0; i < splitIcal.Length; i++)
+            {
+                if (splitIcal[i].Contains(end))
+                {
+                    exportData = false;
+                }
+                if (exportData)
+                {
+                    String[] splitField = splitIcal[i].Split(":");
+                    if (splitField[0].Trim().Equals(fields))
+                    {
+                        if (splitField.Length > 2)
+                        {
+                            String temp = "";
+                            for (int j = 1; j < splitField.Length; j++)
+                            {
+                                temp += splitField[j];
+                            }
+                            if (!values.Contains(temp))
+                                values.Add(temp);
+                        }
+                        else
+                        {
+                            if (!values.Contains(splitField[1]))
+                                values.Add(splitField[1]);
+                        }
+                    }
+                }
+                if (splitIcal[i].Contains(start))
+                {
+                    exportData = true;
+                }
+            }
+
+            return values;
+        }
+
+        private void GetAllSubject(String ical)
+        {
+            List<Subject> subjects = new();
             String[] splitIcal = ical.Split("\n");
             String start = "BEGIN:VEVENT";
             String end = "END:VEVENT";
@@ -79,7 +133,20 @@ namespace PlanningRead
                 }
             }
 
-            return subjects;
+            this.subjects = subjects;
+        }
+
+        public List<Subject> GetByDate(DateTime dateTime)
+        {
+            List<Subject> subjectDate = new();
+
+            foreach (var subject in subjects)
+            {
+                if (subject.GetDateStart().SameDate(dateTime))
+                    subjectDate.Add(subject);
+            }
+
+            return subjectDate;
         }
     }
 }
