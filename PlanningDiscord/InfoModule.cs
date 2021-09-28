faec40344ea3e9866a75c4a4c66e7c58730f1632
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-09-28 22:50:56 +0200

    Display the planning description of a day !planning !planning 8 3 2001

---
+++ PlanningDiscord/InfoModule.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using PlanningRead;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlanningDiscord
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("planning")]
+        public async Task PlanningAsync()
+        {
+            DateTime planningDate = new();
+            PlanningReader planningReader = new("Edt_Licence_3_Miashs_parcours_Miage.ics", "2021.0.1.3", "BE5E4C87C66B25F0E3DA315EA60FC275", "643d5b312e2e36325d2666683d3126663d3131303030");
+            List<Subject> subjects = planningReader.GetByDate(planningDate);
+            int minuteOfWork = PlanningTool.WorkingTime(subjects);
+            int hourWork = minuteOfWork / 60;
+            int minuteWork = minuteOfWork - hourWork * 60;
+            int minuteOfFree = PlanningTool.FreeTime(subjects);
+            int hourFree = minuteOfFree / 60;
+            int minuteFree = minuteOfFree - hourFree * 60;
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Emploi du temps - Hyperplanning",
+                Description = $"Voici l'emploi du temps du **{planningDate.DayToString()} {planningDate.GetDay()} {planningDate.MonthToString()} {planningDate.GetYear()}**",
+                Color = Color.DarkMagenta
+            };
+            embed.AddField("Informations sur la journée",
+                $"Il y a __{hourWork}h{minuteWork} de cours/TD__ et __{hourFree}h{minuteFree} de pause !__")
+                .WithAuthor(Context.Guild.GetUser(239157875342180353))
+                .WithFooter(footer => footer.Text = "Hyperplanning 2021.0.1.3")
+                .WithUrl("http://sco.polytech.unice.fr/1/invite?login=true")
+                .WithCurrentTimestamp();
+
+            await Context.Channel.SendMessageAsync(embed: embed.Build());
+        }
+
+        [Command("planning")]
+        public async Task PlanningAsync( int day, int month, [Remainder] int year)
+        {
+            DateTime planningDate = new(day,month,year);
+            PlanningReader planningReader = new("Edt_Licence_3_Miashs_parcours_Miage.ics", "2021.0.1.3", "BE5E4C87C66B25F0E3DA315EA60FC275", "643d5b312e2e36325d2666683d3126663d3131303030");
+            List<Subject> subjects = planningReader.GetByDate(planningDate);
+            int minuteOfWork = PlanningTool.WorkingTime(subjects);
+            int hourWork = minuteOfWork / 60;
+            int minuteWork = minuteOfWork - hourWork * 60;
+            int minuteOfFree = PlanningTool.FreeTime(subjects);
+            int hourFree = minuteOfFree / 60;
+            int minuteFree = minuteOfFree - hourFree * 60;
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Emploi du temps - Hyperplanning",
+                Description = $"Voici l'emploi du temps du **{planningDate.DayToString()} {planningDate.GetDay()} {planningDate.MonthToString()} {planningDate.GetYear()}**",
+                Color = Color.DarkMagenta
+            };
+            embed.AddField("Informations sur la journée",
+                $"Il y a __{hourWork}h{minuteWork} de cours/TD__ et __{hourFree}h{minuteFree} de pause !__")
+                .WithAuthor(Context.Guild.GetUser(239157875342180353))
+                .WithFooter(footer => footer.Text = "Hyperplanning 2021.0.1.3")
+                .WithUrl("http://sco.polytech.unice.fr/1/invite?login=true")
+                .WithCurrentTimestamp();
+
+            await Context.Channel.SendMessageAsync(embed: embed.Build());
+        }
+    }
+}
--- PlanningDiscord/Program.cs
+++ PlanningDiscord/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace PlanningDiscord
@@ -12,26 +14,55 @@ namespace PlanningDiscord
         => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
+        private CommandService _commands;
+        private readonly IServiceProvider _services;
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
+            DiscordSocketConfig discordSocketConfig = new();
+            discordSocketConfig.LogLevel = LogSeverity.Debug;
+            _client = new DiscordSocketClient(discordSocketConfig);
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+                CaseSensitiveCommands = false,
+            });
 
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            _client.Log += Log;
+            _commands.Log += Log;
+
             var token = File.ReadAllText("discordToken.var");
 
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
+            await InitCommands();
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        private async Task InitCommands()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            int pos = 0;
+            if (msg.HasCharPrefix('!', ref pos) || msg.HasMentionPrefix(_client.CurrentUser, ref pos))
+            {
+                var context = new SocketCommandContext(_client, msg);
+
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
         }
 
         private Task Log(LogMessage msg)
