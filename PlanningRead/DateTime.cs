7be1ca75a84a3e1bd2df0a8d2e9d51d6fc7911f6
Sébastien Aglaé <sebastien.aglae@etu.unice.fr>
2021-09-28 22:51:37 +0200

    Updating the planning reader to support time difference

--- PlanningRead/DateTime.cs
+++ PlanningRead/DateTime.cs
@@ -1,26 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlanningRead
 {
     public class DateTime
     {
-        private int day;
-        private int month;
-        private int year;
-        private int hour;
-        private int minute;
-        private int second;
+        private readonly int day;
+        private readonly int month;
+        private readonly int year;
+        private readonly int hour;
+        private readonly int minute;
+        private readonly int second;
+
+        public DateTime()
+        {
+            System.DateTime dateTimeNow = System.DateTime.Now;
+            this.day = dateTimeNow.Day;
+            this.month = dateTimeNow.Month;
+            this.year = dateTimeNow.Year;
+
+            this.second = 0;
+            this.minute = 0;
+            this.hour = 0;
+        }
+
+        public DateTime(int day, int month, int year)
+        {
+            System.DateTime dateTimeNow = System.DateTime.Now;
+            this.day = day;
+            this.month = month;
+            this.year = year;
+
+            this.second = 0;
+            this.minute = 0;
+            this.hour = 0;
+        }
 
         public DateTime(String datetime, int utc)
         {
-            //20210926T220237Z
-            this.day = Int32.Parse(datetime.Substring(6, 2));
-            this.month = Int32.Parse(datetime.Substring(4, 2));
-            this.year = Int32.Parse(datetime.Substring(0, 4));
+            this.day = int.Parse(datetime.Substring(6, 2));
+            this.month = int.Parse(datetime.Substring(4, 2));
+            this.year = int.Parse(datetime.Substring(0, 4));
 
             if (datetime.Length <= 9)
             {
@@ -30,10 +49,20 @@ namespace PlanningRead
             }
             else
             {
-                this.second = Int32.Parse(datetime.Substring(13, 2));
-                this.minute = Int32.Parse(datetime.Substring(11, 2));
-                this.hour = Int32.Parse(datetime.Substring(9, 2)) + utc;
+                this.second = int.Parse(datetime.Substring(13, 2));
+                this.minute = int.Parse(datetime.Substring(11, 2));
+                this.hour = int.Parse(datetime.Substring(9, 2)) + utc;
             }
+        }
+
+        public int TimeBetween(DateTime dateTime)
+        {
+            return Math.Abs((dateTime.GetHour() * 60 + dateTime.GetMinute()) - (hour * 60 + minute));
+        }
+
+        public bool SameDate(DateTime dateTime)
+        {
+            return SameDate(dateTime.GetDay(), dateTime.GetMonth(), dateTime.GetYear());
         }
 
         public bool SameDate(int day, int month, int year)
@@ -79,6 +108,52 @@ namespace PlanningRead
         public int GetSecond()
         {
             return second;
+        }
+
+        public String MonthToString()
+        {
+            return month switch
+            {
+                1 => "Janvier",
+                2 => "Fevrier",
+                3 => "Mars",
+                4 => "Avril",
+                5 => "Mai",
+                6 => "Juin",
+                7 => "Juillet",
+                8 => "Aout",
+                9 => "Septembre",
+                10 => "Octobre",
+                11 => "Novembre",
+                12 => "Decembre",
+                _ => "<inconnu>",
+            };
+        }
+
+        public string DayToString()
+        {
+            System.DateTime dateTime = new(year, month, day);
+            return dateTime.DayOfWeek switch
+            {
+                DayOfWeek.Sunday => "Dimanche",
+                DayOfWeek.Monday => "Lundi",
+                DayOfWeek.Tuesday => "Mardi",
+                DayOfWeek.Wednesday => "Mercredi",
+                DayOfWeek.Thursday => "Jeudi",
+                DayOfWeek.Friday => "Vendredi",
+                DayOfWeek.Saturday => "Samedi",
+                _ => "<inconnu>",
+            };
+        }
+
+        public String HourMinuteToString()
+        {
+            return hour + ":" + minute;
+        }
+
+        public String DateToString()
+        {
+            return hour + ":" + minute;
         }
 
         public override String ToString()
--- PlanningRead/Program.cs
+++ PlanningRead/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PlanningRead
 {
@@ -6,9 +7,36 @@ namespace PlanningRead
     {
         static void Main(string[] args)
         {
-            String val = PlanningReader.GetIcalContent("http://sco.polytech.unice.fr/1/Telechargements/ical/Edt_Licence_3_Miashs_parcours_Miage.ics?version=2021.0.1.3&idICal=BE5E4C87C66B25F0E3DA315EA60FC275&param=643d5b312e2e36325d2666683d3126663d3131303030");
-            Console.WriteLine(PlanningReader.GetAllSubject(val)[0].GetDateStart());
-            Console.WriteLine(PlanningReader.GetAllSubject(val)[0].GetDateEnd());
+            PlanningReader planningReader = new("Edt_Licence_3_Miashs_parcours_Miage.ics", "2021.0.1.3", "BE5E4C87C66B25F0E3DA315EA60FC275", "643d5b312e2e36325d2666683d3126663d3131303030");
+            foreach (var item in PlanningReader.GetAllFields(PlanningReader.GetIcalContent("Edt_Licence_3_Miashs_parcours_Miage.ics", "2021.0.1.3", "BE5E4C87C66B25F0E3DA315EA60FC275", "643d5b312e2e36325d2666683d3126663d3131303030")))
+            {
+                //Console.WriteLine($"{item}");
+            }
+
+            String[] fields = { "CATEGORIES",
+"DTSTAMP",
+"LAST-MODIFIED",
+"UID",
+"DTSTART",
+"DTEND",
+"SUMMARY;LANGUAGE=fr",
+"LOCATION;LANGUAGE=fr",
+"DESCRIPTION;LANGUAGE=fr",
+"X-ALT-DESC;FMTTYPE=text/html",
+"DTSTART;VALUE=DATE",
+"DTEND;VALUE=DATE"};
+            foreach (var field in fields)
+            {
+                String temp = "";
+                foreach (var item in PlanningReader.GetAllValue(PlanningReader.GetIcalContent("Edt_Licence_3_Miashs_parcours_Miage.ics", "2021.0.1.3", "BE5E4C87C66B25F0E3DA315EA60FC275", "643d5b312e2e36325d2666683d3126663d3131303030"), field))
+                {
+                    Console.WriteLine($"{item}");
+                    temp += item + "\n";
+                }
+                Directory.CreateDirectory("info");
+                File.WriteAllText("info/" + field.Replace(";", "-").Replace("/", "-") + ".txt", temp);
+            }
+
         }
     }
 }
